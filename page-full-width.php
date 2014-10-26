57d50222b6d37826fff86e63f7842a165665e65e
PJ Hampton <hampton-p1@email.ulster.ac.uk>
2014-10-26 17:37:05 +0000

    Fixed Minor Formatting Issues

--- page-full-width.php
+++ page-full-width.php
@@ -1,37 +1,35 @@
 <?php
 /*
-  Template Name: Full Width Template
+  Template Name: Full Width
 */
-
 ?>
+
 <?php get_header(); ?>
 
-  <div class="container">   
+<div class="container">   
     <div class="row">
-      
-      <div class="col-md-12">
+        <div class="col-md-12">
 
-        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+            <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-          <div class="page-header">
-            <h1><?php the_title(); ?></h1>
-          </div>
+            <div class="page-header">
+              <h1><?php the_title(); ?></h1>
+            </div>
 
-          <?php the_content(); ?>
+            <?php the_content(); ?>
 
-        <?php endwhile; else: ?>
+            <?php endwhile; else: ?>
           
-          <div class="page-header">
-            <h1>Oh no!</h1>
-          </div>
+            <div class="page-header">
+              <h1>Oh no!</h1>
+            </div>
 
-          <p>No content is appearing for this page!</p>
+              <p>No content is appearing for this page!</p>
 
-        <?php endif; ?>
+            <?php endif; ?>
 
-
-      </div>      
-
+        </div>      
     </div>
+</div>
 
 <?php get_footer(); ?>
