e2421978a015597d5b1f5271b205f4f7ca9cd1d8
PJ Hampton <hampton-p1@email.ulster.ac.uk>
2014-10-26 16:11:27 +0000

    Added custom JS description

--- js/theme.js
+++ js/theme.js
@@ -1,3 +1,8 @@
+/*
+	BigBooty's Custom JavaScript.
+	Here is some to get you started.
+*/
+
 jQuery(document).ready(function ($) {
   $('[data-toggle=offcanvas]').click(function () {
     $('.row-offcanvas').toggleClass('active')
