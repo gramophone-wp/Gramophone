63f7f5678093b04e2a83236040b218973c6d308c
PJ Hampton <hampton-p1@email.ulster.ac.uk>
2014-10-26 15:06:18 +0000

    Restructured and overhauled header.php
    Made the following changes:
    
    * Add language meta from just English
    * Corrected Formatting. Now reads more cohesive
    * Added Pingback
    * Pulled Menu to right (This may change back)

--- header.php
+++ header.php
@@ -1,7 +1,13 @@
 <!DOCTYPE html>
-<html lang="en">
+
+<!--[if lt IE 7]><html <?php language_attributes(); ?> class="no-js lt-ie9 lt-ie8 lt-ie7"><![endif]-->
+<!--[if (IE 7)&!(IEMobile)]><html <?php language_attributes(); ?> class="no-js lt-ie9 lt-ie8"><![endif]-->
+<!--[if (IE 8)&!(IEMobile)]><html <?php language_attributes(); ?> class="no-js lt-ie9"><![endif]-->
+<!--[if gt IE 8]><!--> <html <?php language_attributes(); ?> class="no-js"><!--<![endif]-->
+
 <head>
-    <meta charset="utf-8">
+
+    <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="shortcut icon" href="<?php bloginfo('template_directory');?>/images/favicon.ico">
@@ -11,6 +17,8 @@
       <?php bloginfo( 'name' ); ?>
     </title>
 
+    <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+
     <?php wp_head(); ?>
 
 </head>
@@ -18,32 +26,33 @@
 <body <?php body_class(); ?>>
 
 
+<!-- Typical Bootstrap Navigation Menu -->
 <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-  <div class="container">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="<?php bloginfo( 'url' ); ?>"><?php bloginfo( 'name' ); ?></a>
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="<?php bloginfo( 'url' ); ?>"><?php bloginfo( 'name' ); ?></a>
+        </div>
+
+        <div class="navbar-collapse collapse pull-right">
+            <?php 
+              $menu_args = array(
+                  'menu'            => 'header-menu', 
+                  'container'       => 'container', 
+                  'container_class' => 'container_class', 
+                  'container_id'    => 'container_id',
+                  'menu_class'      => 'nav navbar-nav',
+                  'menu_id'         => 'menu_id',
+                  'echo'            => true);
+            ?>
+        <?php wp_nav_menu($menu_args);?>
+        </div>
+        
     </div>
-
-    <div class="navbar-collapse collapse">
- <?php 
-    $menu_args = array(
-      'menu'            => 'header-menu', 
-      'container'       => 'container', 
-      'container_class' => 'container_class', 
-      'container_id'    => 'container_id',
-      'menu_class'      => 'nav navbar-nav',
-      'menu_id'         => 'menu_id',
-      'echo'            => true);
-    ?>
-    <?php wp_nav_menu($menu_args);?>
-
-    </div><!--/.navbar-collapse -->
-
-  </div>
 </div>
+<!-- END MENU -->
