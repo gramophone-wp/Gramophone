2b7491d13c4b44894e44a5a31c55a11a8182c5f6
PJ Hampton <hampton-p1@email.ulster.ac.uk>
2014-10-26 17:49:52 +0000

    Fixed paths to css & js files in /lib/

--- functions.php
+++ functions.php
@@ -3,8 +3,8 @@
 function theme_styles() 
 {
 
-	wp_enqueue_style( 'bootstrap_css', get_template_directory_uri() . '/css/bootstrap.min.css' );
-	wp_enqueue_style( 'main_css', get_template_directory_uri() . '/style.css' );
+	wp_enqueue_style( 'bootstrap_css', get_template_directory_uri() . '/lib/css/bootstrap.min.css' );
+	wp_enqueue_style( 'main_css', get_template_directory_uri() . 'style.css' );
 
 }
 add_action( 'wp_enqueue_scripts', 'theme_styles' );
@@ -21,8 +21,8 @@ function theme_js()
 	$wp_scripts->add_data( 'html5_shiv', 'conditional', 'lt IE 9' );
 	$wp_scripts->add_data( 'respond_js', 'conditional', 'lt IE 9' );
 
-	wp_enqueue_script( 'bootstrap_js', get_template_directory_uri() . '/js/bootstrap.min.js', array('jquery'), '', true );
-	wp_enqueue_script( 'theme_js', get_template_directory_uri() . '/js/theme.js', array('jquery', 'bootstrap_js'), '', true );
+	wp_enqueue_script( 'bootstrap_js', get_template_directory_uri() . '/lib/js/bootstrap.min.js', array('jquery'), '', true );
+	wp_enqueue_script( 'theme_js', get_template_directory_uri() . '/lib/js/theme.js', array('jquery', 'bootstrap_js'), '', true );
 
 }
 add_action( 'wp_enqueue_scripts', 'theme_js' );
--- header.php
+++ header.php
@@ -10,7 +10,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="shortcut icon" href="<?php bloginfo('template_directory');?>/images/favicon.ico">
+    <link rel="shortcut icon" href="<?php bloginfo('template_directory');?>/lib/images/favicon.ico">
 
     <title>
       <?php wp_title( '|', true, 'right' ); ?>
