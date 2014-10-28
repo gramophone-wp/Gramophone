ff951017e8ebb70431e689e379abc4fc8085afaa
PJ Hampton <hampton-p1@email.ulster.ac.uk>
2014-10-28 10:55:39 +0000

    Cleaned up code & message

--- lib/framework/admin.php
+++ lib/framework/admin.php
@@ -1,14 +1,14 @@
 <?php
 
+
 /*
 	Change this to your theme's footer
 */
-function bigbooty_footer () 
+function bigbooty_footer() 
 {
-	echo "A <a href='https://github.com/pjhampton/BigBooty' target='_blank'>BigBooty</a> &hearts;'in Theme | ...Designed by you! <strong>:-)</strong></p>";
+	echo "<a href='https://github.com/pjhampton/BigBooty' target='_blank'>BigBooty</a> &hearts;'s You!</p>";
 }
 add_filter('admin_footer_text', 'bigbooty_footer');
-
 
 
 ?>
