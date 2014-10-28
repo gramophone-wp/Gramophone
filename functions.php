470c1fbd01c8d8f2d4841ab3a1edcac7b9d63a7a
PJ Hampton <hampton-p1@email.ulster.ac.uk>
2014-10-28 08:30:37 +0000

    Fixed enqueue bug

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@ function theme_styles()
 {
 
 	wp_enqueue_style( 'bootstrap_css', get_template_directory_uri() . '/lib/css/bootstrap.min.css' );
-	wp_enqueue_style( 'main_css', get_template_directory_uri() . 'style.css' );
+	wp_enqueue_style( 'main_css', get_template_directory_uri() . '/style.css' );
 
 }
 add_action( 'wp_enqueue_scripts', 'theme_styles' );
