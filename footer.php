03897318bb7b248d08eadb253dc0cd2b2b897be5
PJ Hampton <hampton-p1@email.ulster.ac.uk>
2014-10-28 09:00:28 +0000

    Fixed Footer Styling

--- footer.php
+++ footer.php
@@ -1,14 +1,14 @@
 </div>
-	<footer class="footer">
-		<div class="container">
-			<p class="text-muted">
-				&copy; <?php bloginfo('name'); ?> <?php echo date('Y'); ?>. All Rights Reserved
-			</p>
-		</div>
-	</footer>
-</div>
 
-    <?php wp_footer(); ?>
+<footer class="footer">
+	<div class="container">
+		<p class="text-muted">
+			&copy; <?php bloginfo('name'); ?> <?php echo date('Y'); ?>. All Rights Reserved
+		</p>
+	</div>
+</footer>
+
+<?php wp_footer(); ?>
 
 </body>
 </html>
