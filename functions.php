cbb274e9bf6ebb848d8aa39fc86aa2fd3f868109
PJ Hampton <hampton-p1@email.ulster.ac.uk>
2014-10-29 16:01:38 +0000

    Removed Home Page Widgets. No need for them here

--- functions.php
+++ functions.php
@@ -66,10 +66,6 @@ function create_widget( $name, $id, $description )
 
 }
 
-
-create_widget( 'Front Page Left', 'front-left', 'Displays on the left of the homepage' );
-create_widget( 'Front Page Center', 'front-center', 'Displays in the center of the homepage' );
-create_widget( 'Front Page Right', 'front-right', 'Displays on the right of the homepage' );
 create_widget( 'Page Sidebar', 'page', 'Displays on the side of pages with a sidebar' );
 create_widget( 'Blog Sidebar', 'blog', 'Displays on the side of pages in the blog section' );
 
