e655a70a8e0e5e4a72c0267775e0e61ca632b6f0
PJ Hampton <hampton-p1@email.ulster.ac.uk>
2014-10-30 21:13:48 +0000

    Added Theme support

--- lib/framework/plugins.php
+++ lib/framework/plugins.php
@@ -1,57 +1,4 @@
 <?php
-/*
-Author: Who Am I? (Rename Me)
-URL: https://github.com/pjhampton/BigBooty
-*/
-
-
-// Admin Customisations
-require_once( 'lib/framework/admin.php' );
-require_once( 'lib/framework/menus.php' );
-require_once( 'lib/framework/widgets.php' );
-
-// Loads External Libraries - MOVE THIS!
-require_once locate_template('inc/plugins/class-tgm-plugin-activation.php');
-
-
-function theme_styles() 
-{
-
-	wp_enqueue_style( 'bootstrap_css', get_template_directory_uri() . '/lib/css/bootstrap.min.css' );
-	wp_enqueue_style( 'bigbooty_css', get_template_directory_uri() . '/lib/css/bigbooty.css' );	
-	wp_enqueue_style( 'main_css', get_template_directory_uri() . '/style.css' );
-
-}
-add_action( 'wp_enqueue_scripts', 'theme_styles' );
-
-
-function theme_js() 
-{
-
-	global $wp_scripts;
-
-	wp_register_script( 'html5_shiv', 'https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js', '', '', false );
-	wp_register_script( 'respond_js', 'https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js', '', '', false );
-
-	$wp_scripts->add_data( 'html5_shiv', 'conditional', 'lt IE 9' );
-	$wp_scripts->add_data( 'respond_js', 'conditional', 'lt IE 9' );
-
-	wp_enqueue_script( 'bootstrap_js', get_template_directory_uri() . '/lib/js/bootstrap.min.js', array('jquery'), '', true );
-	wp_enqueue_script( 'theme_js', get_template_directory_uri() . '/lib/js/theme.js', array('jquery', 'bootstrap_js'), '', true );
-
-}
-add_action( 'wp_enqueue_scripts', 'theme_js' );
-
-
-
-
-
-
-add_theme_support( 'post-thumbnails' );
-add_theme_support( 'menus' );
-
-
-
 
 /**
  * Register the required plugins for this theme.
@@ -120,6 +67,5 @@ function my_theme_register_required_plugins() {
 	tgmpa( $plugins, $config );
  
 }
-
 
 ?>
---
+++ lib/framework/support.php
@@ -0,0 +1,18 @@
+<?php
+
+
+/*
+
+	For a full list of features please see the following:
+	http://codex.wordpress.org/Function_Reference/add_theme_support
+
+*/
+
+
+add_theme_support( 'post-thumbnails' );
+add_theme_support( 'menus' );
+add_theme_support( 'custom-background' );
+add_theme_support( 'automatic-feed-links' );
+
+
+?>
