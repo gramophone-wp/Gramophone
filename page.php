74b2441ffe4be2d05b71ba85607278d1134e3427
PJ Hampton <hampton-p1@email.ulster.ac.uk>
2014-10-30 23:32:30 +0000

    fixed off-canvas sidebar

--- page.php
+++ page.php
@@ -1,36 +1,34 @@
 <?php get_header(); ?>
 
-  <div class="container">   
+<div class="container">   
     <div class="row row-offcanvas row-offcanvas-right">
-      
-      <div class="col-md-9">
+        <div class="col-md-9">
 
         <p class="pull-right visible-xs">
-            <button type="button" class="btn btn-primary btn-xs" data-toggle="offcanvas">View Sidebar</button>
+            <button type="button" class="btn btn-primary btn-med" data-toggle="offcanvas">View Sidebar</button>
         </p>
 
         <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-          <div class="page-header">
+        <div class="page-header">
             <h1><?php the_title(); ?></h1>
-          </div>
+        </div>
 
-          <?php the_content(); ?>
+        <?php the_content(); ?>
 
         <?php endwhile; else: ?>
           
-          <div class="page-header">
+        <div class="page-header">
             <h1>Oh no!</h1>
-          </div>
+        </div>
 
-          <p>No content is appearing for this page!</p>
+        <p>No content is appearing for this page!</p>
 
         <?php endif; ?>
 
-
-      </div>
+    </div>
       
-      <?php get_sidebar(); ?>
+    <?php get_sidebar(); ?>
 
     </div>
 
