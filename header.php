5811110dee7a248572846beaca1f5dad23298cd1
PJ Hampton <hampton-p1@email.ulster.ac.uk>
2014-11-03 20:01:40 +0000

    Fixed Style Formatting

--- header.php
+++ header.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!doctype html>
 
 <!--[if lt IE 7]><html <?php language_attributes(); ?> class="no-js lt-ie9 lt-ie8 lt-ie7"><![endif]-->
 <!--[if (IE 7)&!(IEMobile)]><html <?php language_attributes(); ?> class="no-js lt-ie9 lt-ie8"><![endif]-->
@@ -7,52 +7,50 @@
 
 <head>
 
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="shortcut icon" href="<?php bloginfo('template_directory');?>/lib/images/favicon.ico">
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="shortcut icon" href="<?php bloginfo('template_directory');?>/lib/images/favicon.ico">
 
-    <title>
-      <?php wp_title( '|', true, 'right' ); ?>
-      <?php bloginfo( 'name' ); ?>
-    </title>
+	<title>
+		<?php wp_title( '|', true, 'right' ); ?>
+		<?php bloginfo( 'name' ); ?>
+	</title>
 
-    <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
 
-    <?php wp_head(); ?>
+	<?php wp_head(); ?>
 
 </head>
 
 <body <?php body_class(); ?>>
 
-
-<!-- Typical Bootstrap Navigation Menu -->
 <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-    <div class="container">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="<?php bloginfo( 'url' ); ?>"><?php bloginfo( 'name' ); ?></a>
-        </div>
+	<div class="container">
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" href="<?php bloginfo( 'url' ); ?>">
+				<?php bloginfo( 'name' ); ?>
+			</a>
+		</div>
 
-        <div class="navbar-collapse collapse pull-right">
-            <?php 
-              $menu_args = array(
-                  'menu'            => 'header-menu', 
-                  'container'       => 'container', 
-                  'container_class' => 'container_class', 
-                  'container_id'    => 'container_id',
-                  'menu_class'      => 'nav navbar-nav',
-                  'menu_id'         => 'menu_id',
-                  'echo'            => true);
-            ?>
-        <?php wp_nav_menu($menu_args);?>
-        </div>
-        
-    </div>
+		<div class="navbar-collapse collapse pull-right">
+			<?php 
+			$menu_args = array(
+			'menu'            => 'header-menu', 
+			'container'       => 'container', 
+			'container_class' => 'container_class', 
+			'container_id'    => 'container_id',
+			'menu_class'      => 'nav navbar-nav',
+			'menu_id'         => 'menu_id',
+			'echo'            => true);
+			?>
+			<?php wp_nav_menu($menu_args);?>
+		</div>
+	</div>
 </div>
-<!-- END MENU -->
