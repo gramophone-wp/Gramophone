7000e07dba6866a69b006f109d29d024693ba091
PJ Hampton <hampton-p1@email.ulster.ac.uk>
2014-11-04 21:18:54 +0000

    Added Responsive Elements to Footer

--- footer.php
+++ footer.php
@@ -1,8 +1,10 @@
 <footer class="footer">
 	<div class="container">
-		<p class="text-muted">
-			&copy; <?php bloginfo('name'); ?> <?php echo date('Y'); ?>. All Rights Reserved
-		</p>
+		<div class="col-xs-12 col-md-6" id="copyright">
+			<p class="text-muted">
+				&copy; <?php bloginfo('name'); ?> <?php echo date('Y'); ?>. All Rights Reserved
+			</p>
+		</div>
 	</div>
 </footer>
 
