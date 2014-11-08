572180b19db933b9d76d56275f87e4fcec2f6628
PJ Hampton <hampton-p1@email.ulster.ac.uk>
2014-11-08 22:34:56 +0000

    Added the_title() function to front-page

--- front-page.php
+++ front-page.php
@@ -3,6 +3,8 @@
 <div class="jumbotron">
 	<div class="container">
 
+	<h1><?php the_title(); ?></h1>
+
 	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
 	<?php the_content(); ?>
