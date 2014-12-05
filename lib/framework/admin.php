b34c47cb1421b6225ebd22eee54353df4fbda343
PJ Hampton <hampton-p1@email.ulster.ac.uk>
2014-12-05 22:50:33 +0000

    Added function for Login Screen Customisation

--- lib/framework/admin.php
+++ lib/framework/admin.php
@@ -1,14 +1,15 @@
 <?php
 
-
 /*
-	Change this to your theme's footer
+	Custom Login Screen
+	Codex: http://codex.wordpress.org/Customizing_the_Login_Form
 */
-function bigbooty_footer() 
+
+function bigbooty_login() 
 {
-	echo "<a href='https://github.com/pjhampton/BigBooty' target='_blank'>BigBooty</a> &hearts;'s You!</p>";
+    wp_enqueue_style( 'bigbooty-login', get_template_directory_uri() . '/admin/login/login.css' );
 }
-add_filter('admin_footer_text', 'bigbooty_footer');
+add_action( 'login_enqueue_scripts', 'bigbooty_login' );
 
 
 /*
@@ -25,5 +26,16 @@ function bigbooty_admin_colors()
 	);
 }
 add_action('admin_init', 'bigbooty_admin_colors');
+
+
+/*
+	Change this to your theme's footer
+*/
+	
+function bigbooty_footer() 
+{
+	echo "<a href='https://github.com/pjhampton/BigBooty' target='_blank'>BigBooty</a> &hearts;'s You!</p>";
+}
+add_filter('admin_footer_text', 'bigbooty_footer');
 
 ?>
