59bb464e65d4a509a065c4f9624079e97d26b90c
PJ Hampton <hampton-p1@email.ulster.ac.uk>
2015-01-09 10:54:41 +0000

    Added author stub

---
+++ author.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Summary
+ *
+ * The template for displaying Author Related Posts & Content
+ *
+ * @since BigBooty 1.0.0
+ */
+?>
+
+<?php get_header(); ?>
+
+<div class="container">
+	<div class="row" id="authorDetails">
+
+	</div>
+</div>
+
+<?php get_footer(); ?>
