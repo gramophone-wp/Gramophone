ff719800b47be1bf7dbb7d875ca4bda7b4fe4f87
PJ Hampton <hampton-p1@email.ulster.ac.uk>
2015-01-11 16:03:43 +0000

    json-ld sitelinks search annotation

--- footer.php
+++ footer.php
@@ -8,6 +8,19 @@
 	</div>
 </footer>
 
+<script type="application/ld+json">
+{
+   "@context": "http://schema.org",
+   "@type": "WebSite",
+   "url": "https://www.example.com/",
+   "potentialAction": {
+     "@type": "SearchAction",
+     "target": "http://example.com/?s={search_term_string}",
+     "query-input": "required name=search_term_string"
+   }
+}
+</script>
+
 <?php wp_footer(); ?>
 
 </body>
