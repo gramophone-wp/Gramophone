eec6e3504a1a98e29517ed7eef1eb2579f171f45
PJ Hampton <hampton-p1@email.ulster.ac.uk>
2015-02-19 12:03:17 +0000

    Added woocommerce_content() func

--- woocommerce.php
+++ woocommerce.php
@@ -20,23 +20,7 @@
             <button type="button" class="btn btn-primary btn-med" data-toggle="offcanvas">View Sidebar</button>
         </p>
 
-        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-
-        <div class="page-header">
-            <h1><?php the_title(); ?></h1>
-        </div>
-
-        <?php the_content(); ?>
-
-        <?php endwhile; else: ?>
-          
-        <div class="page-header">
-            <h1>Oh no!</h1>
-        </div>
-
-        <p>No content is appearing for this page!</p>
-
-        <?php endif; ?>
+        <?php woocommerce_content(); ?>
 
         </div>
       
