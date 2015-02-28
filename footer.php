2e3fd189ab5cdf06046b27bca5a094135ff51105
PJ Hampton <hampton-p1@email.ulster.ac.uk>
2015-02-28 20:57:04 +0000

    fixed basic footer

--- footer.php
+++ footer.php
@@ -13,9 +13,7 @@
 <footer class="footer">
 	<div class="container">
 		<div class="col-xs-12 col-md-6" id="copyright">
-			<p class="text-muted">
-				&copy; <?php bloginfo('name'); ?> <?php echo date('Y'); ?>. All Rights Reserved
-			</p>
+			<p class="text-muted">&copy; <?php bloginfo('name'); ?> <?php echo date('Y'); ?>. All Rights Reserved</p>
 		</div>
 	</div>
 </footer>
