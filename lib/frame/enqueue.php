5842753978a9392ed1c67a93ece44fcd2a35127d
PJ Hampton <hampton-p1@email.ulster.ac.uk>
2015-04-04 16:15:26 +0100

    updated enqueued files

--- lib/frame/enqueue.php
+++ lib/frame/enqueue.php
@@ -10,7 +10,7 @@
 function bigbooty_styles() 
 {
 
-    wp_enqueue_style( 'bootstrap_css', get_template_directory_uri() . '/lib/css/bootstrap.min.css' );
+    wp_enqueue_style( 'bootstrap_css', get_template_directory_uri() . '/bower_components/bootstrap/dist/css/bootstrap.min.css' );
     wp_enqueue_style( 'bigbooty_css', get_template_directory_uri() . '/lib/css/bigbooty.css' );	
     wp_enqueue_style( 'main_css', get_template_directory_uri() . '/style.css' );
 
@@ -36,7 +36,7 @@ function bigbooty_js()
     $wp_scripts->add_data( 'html5_shiv', 'conditional', 'lt IE 9' );
     $wp_scripts->add_data( 'respond_js', 'conditional', 'lt IE 9' );
 
-    wp_enqueue_script( 'bootstrap_js', get_template_directory_uri() . '/lib/js/bootstrap.min.js', array('jquery'), '', true );
+    wp_enqueue_script( 'bootstrap_js', get_template_directory_uri() . '//bower_components/bootstrap/dist/js/bootstrap.min.js', array('jquery'), '', true );
     wp_enqueue_script( 'theme_js', get_template_directory_uri() . '/lib/js/theme.js', array('jquery', 'bootstrap_js'), '', true );
 
     // Add more...
