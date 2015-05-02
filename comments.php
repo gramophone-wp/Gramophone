ed70ec28eeaa8855895b916769698aed382353ca
PJ Hampton <hampton-p1@email.ulster.ac.uk>
2015-05-02 20:16:49 +0100

    Added comments.php

---
+++ comments.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Summary
+ * @copyright 2014 - 2015
+ *
+ * The template for handling the themes comments
+ *
+ * @author pjh
+ * @link https://codex.wordpress.org/Function_Reference/comments_template
+ * @todo Implement the code
+ * @since BigBooty 1.0.0
+ */
+
+echo "Hello, Comments";
+
+?>
