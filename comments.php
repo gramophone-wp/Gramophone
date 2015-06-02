03079baad419c96aeb11684fa4683811a2f55084
PJ Hampton <hampton-p1@email.ulster.ac.uk>
2015-06-02 17:14:56 +0100

    Light refactoring work on comments.php

--- comments.php
+++ comments.php
@@ -7,10 +7,9 @@
  *
  * @author pjh
  * @link https://codex.wordpress.org/Function_Reference/comments_template
- * @todo Implement the code
+ * @todo Fix up the code from shift
  * @since BigBooty 1.0.0
  */
-
 
 
 if ( post_password_required() )
@@ -19,9 +18,8 @@ if ( post_password_required() )
 
 <div id="comments" class="comments-area">
 
-    <?php // You can start editing here -- including this comment! ?>
-
     <?php if ( have_comments() ) : ?>
+
         <h2 class="comments-title">
             <?php
                 printf( _n( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'twentytwelve' ),
@@ -31,28 +29,29 @@ if ( post_password_required() )
 
         <ol class="commentlist">
             <?php wp_list_comments( array( 'callback' => 'twentytwelve_comment', 'style' => 'ol' ) ); ?>
-        </ol><!-- .commentlist -->
+        </ol>
 
-        <?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through ?>
+        <?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : ?>
+
         <nav id="comment-nav-below" class="navigation" role="navigation">
             <h1 class="assistive-text section-heading"><?php _e( 'Comment navigation', 'twentytwelve' ); ?></h1>
             <div class="nav-previous"><?php previous_comments_link( __( '&larr; Older Comments', 'twentytwelve' ) ); ?></div>
             <div class="nav-next"><?php next_comments_link( __( 'Newer Comments &rarr;', 'twentytwelve' ) ); ?></div>
         </nav>
-        <?php endif; // check for comment navigation ?>
+
+        <?php endif; ?>
 
         <?php
-        /* If there are no comments and comments are closed, let's leave a note.
-         * But we only want the note on posts and pages that had comments in the first place.
-         */
         if ( ! comments_open() && get_comments_number() ) : ?>
         <p class="nocomments"><?php _e( 'Comments are closed.' , 'twentytwelve' ); ?></p>
         <?php endif; ?>
 
     <?php endif; // have_comments() ?>
 
-    <?php //comment_form(); ?>
+
+    <!-- Comment Form -->
     <?php if ('open' == $post->comment_status) : ?> 
+
     <div id="respond" class="comment-respond">       
         <h3 id="reply-title" class="comment-reply-title"><?php comment_form_title( 'Leave a Reply', 'Leave a Reply to %s' ); ?></h3>
         <div id="cancel-comment-reply">
@@ -85,20 +84,18 @@ if ( post_password_required() )
 
                 <?php endif; ?>
 
-                <!--<p class="form-allowed-tags"><small><strong>XHTML:</strong> You can use these tags: <code><?php echo allowed_tags(); ?></code></small></p>-->
-
                     <p class="comment-form-comment"><p><label for="comment">Comment <?php if ($req) echo '<span class="required">*</span>'; ?></label>
                     <textarea name="comment" id="comment" cols="100%" rows="10" tabindex="4"></textarea></p>
 
                     <p class="form-submit"><input class="btn btn-primary" name="submit" type="submit" id="submit" tabindex="5" value="Post Comment" />
                     <?php comment_id_fields(); ?>
                     </p>
-                    <?php do_action('comment_form', $post->ID); ?>           
+                    <?php do_action('comment_form', $post->ID); ?>   
+
             </form>      
         <?php endif; // If registration required and not logged in ?>
     </div>
 <?php endif; // if you delete this the sky will fall on your head ?>
 
 
-
-</div><!-- #comments .comments-area -->
+</div>
