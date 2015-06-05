c9131f091b1f71b1e312d13df23a8374620264ac
PJ Hampton <hampton-p1@email.ulster.ac.uk>
2015-06-05 23:10:06 +0100

    Fixed callback bug

--- comments.php
+++ comments.php
@@ -27,7 +27,7 @@ if ( post_password_required() )
         </h2>
 
         <ol class="commentlist">
-            <?php wp_list_comments( array( 'callback' => 'twentytwelve_comment', 'style' => 'ol' ) ); ?>
+            <?php wp_list_comments( array( 'style' => 'ol' ) ); ?>
         </ol>
 
         <?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : ?>
