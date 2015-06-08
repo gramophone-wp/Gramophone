619b6b6bccfecc5656ccec66ecb4efb1a9a34035
PJ Hampton <hampton-p1@email.ulster.ac.uk>
2015-06-08 09:46:57 +0100

    Added comments function callback
    Add function callback to style the comments section on the posts page.
    I also increased the line count to improve readability.

--- functions.php
+++ functions.php
@@ -10,22 +10,28 @@
  * @since BigBooty 1.0.0
  */
 
+
 // Theme Dependencies
 require_once locate_template('inc/plugins/class-tgm-plugin-activation.php');
 require_once ( 'inc/plugins/wp_bootstrap_navwalker.php' );
 
+
 // Admin Customisations
 require_once( 'lib/frame/admin.php' );
+
 
 // Theme Specific Customisations
 require_once( 'lib/frame/enqueue.php' );
 require_once( 'lib/frame/menus.php' );
 require_once( 'lib/frame/widgets.php' );
+require_once( 'lib/frame/comments.php' );
 require_once( 'lib/frame/support.php' );
 require_once( 'lib/frame/breadcrumbs.php' );
 
+
 // Theme Recommended & Required Plugins
 require_once( 'lib/frame/required_plugins.php' );
+
 
 // Theme custom post types
 require_once( 'lib/frame/customs/portfolio.php' );
