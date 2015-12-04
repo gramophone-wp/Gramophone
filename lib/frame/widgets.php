735587c7c7ad0c790d057507bfd63496733548cb
PJ Hampton <hampton-p1@email.ulster.ac.uk>
2015-12-04 21:54:31 +0000

    Added footer widgets

--- lib/frame/widgets.php
+++ lib/frame/widgets.php
@@ -19,16 +19,17 @@ function create_widget( $name, $id, $description )
         'description' => __( $description ),
         'before_widget' => '<div class="widget">',
         'after_widget' => '</div>',
-        'before_title' => '<h3>',
-        'after_title' => '</h3>'
+        'before_title' => '<h4 class="text-center">',
+        'after_title' => '</h4>'
     ));
 }
 
-create_widget( 'Front Page Left', 'front-left', 'Displays on the side of pages with a sidebar' );
-create_widget( 'Front Page Center', 'front-center', 'Displays on the side of pages with a sidebar' );
-create_widget( 'Front Page Right', 'front-right', 'Displays on the side of pages with a sidebar' );
 create_widget( 'Page Sidebar', 'page', 'Displays on the side of pages with a sidebar' );
 create_widget( 'Blog Sidebar', 'blog', 'Displays on the side of pages in the blog section' );
+
+create_widget( 'Footer Left', 'footer_left', 'This is the left footer' );
+create_widget( 'Footer Center', 'footer_center', 'This is the center footer' );
+create_widget( 'Footer Right', 'footer_right', 'This is the right footer' );
 // ...add more
 
 ?>
