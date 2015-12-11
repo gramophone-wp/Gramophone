956d91ee002f571d9b651c4b04c3bffb18d92c9a
PJ Hampton <hampton-p1@email.ulster.ac.uk>
2015-12-11 22:52:28 +0000

    moved frame to root, separate from lib/

--- functions.php
+++ functions.php
@@ -17,23 +17,23 @@ require_once ( 'includes/wp_bootstrap_navwalker.php' );
 
 
 // Admin Customisations
-require_once( 'lib/frame/admin.php' );
+require_once( 'frame/admin.php' );
 
 
 // Theme Specific Customisations
-require_once( 'lib/frame/enqueue.php' );
-require_once( 'lib/frame/menus.php' );
-require_once( 'lib/frame/widgets.php' );
-require_once( 'lib/frame/comments.php' );
-require_once( 'lib/frame/support.php' );
-require_once( 'lib/frame/breadcrumbs.php' );
+require_once( 'frame/enqueue.php' );
+require_once( 'frame/menus.php' );
+require_once( 'frame/widgets.php' );
+require_once( 'frame/comments.php' );
+require_once( 'frame/support.php' );
+require_once( 'frame/breadcrumbs.php' );
 
 
 // Theme Recommended & Required Plugins
-require_once( 'lib/frame/required_plugins.php' );
+require_once( 'frame/required_plugins.php' );
 
 
 // Theme custom post types
-require_once( 'lib/frame/customs/portfolio.php' );
+require_once( 'frame/customs/portfolio.php' );
 
 ?>
