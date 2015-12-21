93bd07378b3f6c2c4a11f8a435a4e9d4d775af6d
PJ Hampton <hampton-p1@email.ulster.ac.uk>
2015-12-21 22:53:47 +0000

    removed sidebar (bloat) from home.php

--- home.php
+++ home.php
@@ -25,7 +25,7 @@
 
 <div class="container">   
     <div class="row">
-        <div class="col-md-8">
+        <div class="col-md-12">
 
             <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
@@ -56,8 +56,6 @@
             <?php endif; ?>
 
         </div>
-
-    <?php get_sidebar( 'blog' ); ?>
 
     </div>
 </div>
