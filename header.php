3716af41e0614f4778c95eacaed6dcc906a6f60c
PJ Hampton <hampton-p1@email.ulster.ac.uk>
2016-01-03 22:32:20 +0000

    corrected favicon path

--- header.php
+++ header.php
@@ -28,7 +28,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="shortcut icon" href="<?php bloginfo('template_directory');?>/lib/images/favicon.ico">
+    <link rel="shortcut icon" href="<?php bloginfo('template_directory');?>/lib/images/favicons/favicon.ico">
 
     <title>
         <?php wp_title( '|', true, 'right' ); ?>
