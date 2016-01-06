2876499fab27d785efe7a71352c1cbdf29faa263
PJ Hampton <hampton-p1@email.ulster.ac.uk>
2016-01-06 17:39:20 +0000

    added jumbotron into the archive

--- archive.php
+++ archive.php
@@ -18,13 +18,19 @@
 
 <?php get_header(); ?>
 
+<div class="jumbotron">
+    <div class="container">
+
+        <h1><?php the_title(); ?></h1>
+
+    </div>
+</div>
+
 <div class="container">   
     <div class="row">
+
         <div class="col-md-9">
 
-            <div class="page-header">
-                <h1><?php wp_title(''); ?></h1>
-            </div>
 
             <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
