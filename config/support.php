eb5e190189e7390cf4cc05b69484b98a908f43e6
PJ Hampton <hampton-p1@email.ulster.ac.uk>
2016-02-13 21:45:24 +0000

    Added remaining support features.

--- config/support.php
+++ config/support.php
@@ -15,6 +15,7 @@
  * @version
 **/
 
+
 /**
  **********************************************
  * add_theme_support/0
@@ -25,10 +26,16 @@
  * 
 **/
 
-add_theme_support( 'post-thumbnails' );
-add_theme_support( 'menus' );
-add_theme_support( 'custom-background' );
 add_theme_support( 'automatic-feed-links' );
-// ...add more
+add_theme_support( 'custom-background' );
+// add_theme_support( 'custom-header' );
+// add_theme_support( 'editor-style' );
+// add_theme_support( 'html5' );
+add_theme_support( 'menus' );
+add_theme_support( 'post-thumbnails' );
+// add_theme_support( 'post-formats' );
+// add_theme_support( 'title-tag' );
+// add_theme_support( 'widgets' );
+// add_theme_support();
 
 ?>
