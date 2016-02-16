36a2198b8d89ee8bf95457f668f21e5d75d78618
PJ Hampton <hampton-p1@email.ulster.ac.uk>
2016-02-16 17:15:23 +0000

    Added ability to remove junk from dashboard.

--- config/admin.php
+++ config/admin.php
@@ -83,6 +83,37 @@ add_action('admin_init', 'admin_colors');
 
 /**
  **********************************************
+ * clean_dashboard/0
+ ********************************************** 
+ *
+ * WordPress by default comes with a lot of 
+ * crud. Let's clean up the admin dashboard.
+ * 
+**/
+function clean_dashboard(){
+    global $wp_meta_boxes;
+
+    // unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_right_now']);    // Right Now Widget
+    unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_activity']);        // Activity Widget
+    unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_recent_comments']); // Comments Widget
+    unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_incoming_links']);  // Incoming Links Widget
+    unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_plugins']);         // Plugins Widget
+    // unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_quick_press']);    // Quick Press Widget
+    unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_recent_drafts']);     // Recent Drafts Widget
+    unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_primary']);
+    unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_secondary']);
+
+    // remove plugin dashboard boxes
+    unset($wp_meta_boxes['dashboard']['normal']['core']['yoast_db_widget']);           // Yoast's SEO Plugin Widget
+    unset($wp_meta_boxes['dashboard']['normal']['core']['rg_forms_dashboard']);        // Gravity Forms Plugin Widget
+    unset($wp_meta_boxes['dashboard']['normal']['core']['bbp-dashboard-right-now']);   // bbPress Plugin Widget
+
+}
+add_action( 'wp_dashboard_setup', 'clean_dashboard' );
+
+
+/**
+ **********************************************
  * admin_footer/0
  ********************************************** 
  *
@@ -93,7 +124,7 @@ add_action('admin_init', 'admin_colors');
 	
 function admin_footer() 
 {
-    echo "Built with <a href='https://github.com/pjhampton/BigBooty' target='_blank'>BigBooty Starter Theme</a>";
+    echo "Built with <a href='https://github.com/pjhampton/BigBooty' target='_blank'>BigBooty</a>, the Bootstrap starter theme.";
 }
 add_filter('admin_footer_text', 'admin_footer');
 
