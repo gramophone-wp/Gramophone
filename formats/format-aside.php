884098dd4572658116fa373ccf51fc88dd79e779
PJ Hampton <hampton-p1@email.ulster.ac.uk>
2016-02-16 17:14:34 +0000

    Added working stubs to format files.

--- formats/format-aside.php
+++ formats/format-aside.php
@@ -14,3 +14,12 @@
  * @since
  * @version
 **/
+?>
+
+<article>
+  <h1><?php the_title(); ?></h1>
+
+  <section>
+    <?php the_content(); ?>
+  </section>
+</article>
--- single.php
+++ single.php
@@ -29,55 +29,10 @@
     </div>
 </div>
 
-<div class="container">   
-    <div class="row">
-	<div class="col-md-9 text-justify">
+<div class="container">
+    <?php get_template_part( 'formats/format', get_post_format() ); ?>
+</div>
 
-        <div class="page-header"> 
-
-            <?php
-                $thumbnail_id = get_post_thumbnail_id(); 
-                $thumbnail_url = wp_get_attachment_image_src( $thumbnail_id, 'thumbnail-size', true );
-                $thumbnail_meta = get_post_meta( $thumbnail_id, '_wp_attachment_image_alt', true);                
-            ?>
-
-            <?php if( has_post_thumbnail() ): ?>
-            <p class="featured-image">
-                <img src="<?php echo $thumbnail_url[0]; ?>" alt="<?php echo $thumbnail_meta; ?>">
-            </p>
-            <?php endif; ?>
-
-            <p class="post-meta">
-                By  <span class="vcard author">
-                        <span class="fn"><?php the_author_posts_link(); ?></span> 
-                    </span>
-                on  <span class="date updated"><?php echo the_time('l, F jS, Y');?></span>
-                in <?php the_category( ', ' ); ?>.
-                <a href="<?php comments_link(); ?>" class="comments-number"><?php comments_number(); ?></a>
-            </p>
-        </div>
-
-        <?php the_content(); ?>
-
-        <hr>
-
-        <?php comments_template(); ?>
-
-		<?php endwhile; else: ?>
-          
-        <div class="page-header">
-            <h1>Oh no!</h1>
-        </div>
-
-        <p>No content is appearing for this page!</p>
-
-        <?php endif; ?>
-
-        </div>
-      
-    <?php get_sidebar( 'blog' ); ?>
-
-    </div>
-</div>	
+<?php endwhile; endif; ?>
 
 <?php get_footer(); ?>
