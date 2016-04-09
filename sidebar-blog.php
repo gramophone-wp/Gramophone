b8e90061cc78c46f41ed8e6d745ee61817a29b34
PJ Hampton <hampton-p1@email.ulster.ac.uk>
2016-04-09 09:57:19 +0100

    removed dummy text from sidebars

--- sidebar-blog.php
+++ sidebar-blog.php
@@ -19,10 +19,6 @@
 <div class="col-md-3" id="sidebar">
 
     <?php if ( ! dynamic_sidebar( 'blog' ) ): ?>
-
-    <h3>Sidebar (Blog)</h3>
-    <p>...</p>
-
     <?php endif; ?>
 
 </div>
--- sidebar.php
+++ sidebar.php
@@ -19,10 +19,6 @@
 <div class="col-md-offset-1 col-md-4" id="sidebar">
 
     <?php if ( !dynamic_sidebar( 'page' ) ): ?>
-
-    <h3>Sidebar (Page)</h3>
-    <p>...</p>	
-
     <?php endif; ?>
 
 </div>
