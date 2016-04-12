18f169555c5272e8d16427c8d69f20e79452dded
PJ Hampton <hampton-p1@email.ulster.ac.uk>
2016-04-12 09:01:21 +0100

    refactored author.php

--- author.php
+++ author.php
@@ -28,83 +28,83 @@ $user_twitter = get_the_author_meta('twitter');
 <?php get_header(); ?>
 
 <div class="jumbotron">
-    <div class="container">
-        <div class="col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
-         <div class="profile">
-            <div class="col-sm-12">
-                <div class="col-xs-12 col-sm-8">
-                    <h2><?php the_author(); ?></h2>
+  <div class="container">
+    <div class="col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
+      <div class="profile">
+        <div class="col-sm-12">
+          <div class="col-xs-12 col-sm-8">
+            <h2><?php the_author(); ?></h2>
 
-                    <p><i class="fa fa-quote-left"></i>
-                        <?php echo $biography; ?>
-                    <i class="fa fa-quote-right"></i></p>
+            <p><i class="fa fa-quote-left"></i>
+            <?php echo $biography; ?>
+            <i class="fa fa-quote-right"></i></p>
 
-                    <hr>
-                </div>             
-                <div class="col-xs-12 col-sm-4 text-center">
-                    <img src="<?php echo get_avatar_url( $user_id ); ?>" alt="" class="img-circle img-responsive">
-                </div>
-            </div>            
-            <div class="col-xs-12 divider text-center">
-                <div class="col-xs-12 col-sm-4 emphasis">
-                    <h2><strong> <?php echo count_user_posts(); ?> </strong></h2>                    
-                    <p><small>Posts</small></p>
-                    <button class="btn btn-success btn-block"><span class="fa fa-google-plus"></span> Google Plus </button>
-                </div>
-                <div class="col-xs-12 col-sm-4 emphasis">
-                    <h2><strong>245</strong></h2>                    
-                    <p><small>Following</small></p>
-                    <button class="btn btn-info btn-block"><span class="fa fa-twitter"></span> Twitter </button>
-                </div>
-                <div class="col-xs-12 col-sm-4 emphasis">
-                    <h2><strong>43</strong></h2>                    
-                    <p><small>Snippets</small></p>
-                    <button type="button" class="btn btn-primary btn-block"><span class="fa fa-gear"></span> Options </button>
-                </div>
-            </div>
-         </div>                 
+            <hr>
+          </div>             
+        <div class="col-xs-12 col-sm-4 text-center">
+          <img src="<?php echo get_avatar_url( $user_id ); ?>" alt="" class="img-circle img-responsive">
         </div>
-    </div>
+    
+        </div>            
+        <div class="col-xs-12 divider text-center">
+          <div class="col-xs-12 col-sm-4 emphasis">
+            <h2><strong> <?php echo count_user_posts(); ?> </strong></h2>                    
+            <p><small>Posts</small></p>
+            <button class="btn btn-success btn-block"><span class="fa fa-google-plus"></span> Google Plus </button>
+          </div>
+          <div class="col-xs-12 col-sm-4 emphasis">
+            <h2><strong>245</strong></h2>                    
+            <p><small>Following</small></p>
+            <button class="btn btn-info btn-block"><span class="fa fa-twitter"></span> Twitter </button>
+          </div>
+           <div class="col-xs-12 col-sm-4 emphasis">
+             <h2><strong>43</strong></h2>                    
+             <p><small>Snippets</small></p>
+             <button type="button" class="btn btn-primary btn-block"><span class="fa fa-gear"></span> Options </button>
+           </div>
+         </div>
+       </div>                 
+     </div>
+  </div>
 </div>
 
 <div class="container">
+  <div class="posts">
 
-	<div class="posts">
+  <?php 
+    $curauth = (
+      isset($_GET['author_name'])) 
+        ? get_user_by('slug', $author_name) : get_userdata(intval($author)
+      );
+  ?>
 
-    <?php 
-        $curauth = (
-            isset($_GET['author_name'])) 
-                ? get_user_by('slug', $author_name) : get_userdata(intval($author)
-        );
-    ?>
+  <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-
-    <article class="post">
+  <article class="post">
 
     <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
     <p><em>
-        By <?php the_author(); ?> 
-        on <?php echo the_time('l, F jS, Y');?>
-        in <?php the_category( ', ' ); ?>.
-        <a href="<?php comments_link(); ?>"><?php comments_number(); ?></a>
+      By <?php the_author(); ?> 
+      on <?php echo the_time('l, F jS, Y');?>
+      in <?php the_category( ', ' ); ?>.
+      <a href="<?php comments_link(); ?>"><?php comments_number(); ?></a>
     </em></p>            
 
     <?php the_excerpt(); ?>
 
-    </article>
+  </article>
 
-    <?php endwhile; else: ?>
+  <?php endwhile; else: ?>
 
-    <div class="page-header">
-        <h1>Oh no!</h1>
-    </div>
+  <div class="page-header">
+    <h1>Oh no!</h1>
+  </div>
 
-    <p>No content is appearing for this page!</p>
+  <p>No content is appearing for this page!</p>
 
-    <?php endif; ?>
+  <?php endif; ?>
 
-    </div>
+  </div>
 </div>
 
 <?php get_footer(); ?>
