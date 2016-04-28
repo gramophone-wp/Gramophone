977eeb6228dae64028e7ed432d8212c673a14cb1
PJ Hampton <hampton-p1@email.ulster.ac.uk>
2016-04-28 21:58:06 +0100

    Added example template.

---
+++ templates/page-template.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ **********************************************
+ * Template Name: Example Template 
+ **********************************************
+ * templates/page-template.php
+ **********************************************
+ *
+ * An example template for creating custom 
+ * theme templates.
+ *
+ * @author
+ * @copyright
+ * @link
+ * @todo
+ * @license
+ * @since
+ * @version
+**/
+?>
+
+<?php get_header(); ?>
+
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+<div class="jumbotron">
+  <div class="container">
+    <h1><?php the_title(); ?></h1>
+  </div>
+</div>
+
+<div class="container">   
+  <div class="row">
+    <div class="col-md-12 text-justify">
+
+    <?php the_content(); ?>
+
+    <?php endwhile; else: ?>
+          
+    <div class="page-header">
+      <h1>No Booty Here!</h1>
+    </div>
+
+    <p>There doesn't seem to be anything here as yet!</p>
+
+    <?php endif; ?>
+
+    </div>      
+  </div>
+</div>
+
+<?php get_footer(); ?>
