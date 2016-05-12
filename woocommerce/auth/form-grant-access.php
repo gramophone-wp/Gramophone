67000c66bcbf36b133586e1b29d573e951c3a04b
PJ Hampton <hampton-p1@email.ulster.ac.uk>
2016-05-12 13:19:26 +0100

    Updated formatting for woocommerce auth

--- woocommerce/auth/form-grant-access.php
+++ woocommerce/auth/form-grant-access.php
@@ -16,7 +16,7 @@
 **/
 
 if ( ! defined( 'ABSPATH' ) ) {
-	exit;
+  exit;
 }
 ?>
 
@@ -29,19 +29,19 @@ if ( ! defined( 'ABSPATH' ) ) {
 <p><?php printf( __( 'This will give "%s" <strong>%s</strong> access which will allow it to:' , 'woocommerce' ), esc_html( $app_name ), esc_html( $scope ) ); ?></p>
 
 <ul class="wc-auth-permissions">
-	<?php foreach ( $permissions as $permission ) : ?>
-		<li><?php echo esc_html( $permission ); ?></li>
-	<?php endforeach; ?>
+  <?php foreach ( $permissions as $permission ) : ?>
+    <li><?php echo esc_html( $permission ); ?></li>
+  <?php endforeach; ?>
 </ul>
 
 <div class="wc-auth-logged-in-as">
-	<?php echo get_avatar( $user->ID, 70 ); ?>
-	<p><?php printf( __( 'Logged in as %s', 'woocommerce' ), esc_html( $user->display_name ) ); ?> <a href="<?php echo esc_url( $logout_url ); ?>" class="wc-auth-logout"><?php _e( 'Logout', 'woocommerce' ); ?></a>
+  <?php echo get_avatar( $user->ID, 70 ); ?>
+  <p><?php printf( __( 'Logged in as %s', 'woocommerce' ), esc_html( $user->display_name ) ); ?> <a href="<?php echo esc_url( $logout_url ); ?>" class="wc-auth-logout"><?php _e( 'Logout', 'woocommerce' ); ?></a>
 </div>
 
 <p class="wc-auth-actions">
-	<a href="<?php echo esc_url( $granted_url ); ?>" class="button button-primary wc-auth-approve"><?php _e( 'Approve', 'woocommerce' ); ?></a>
-	<a href="<?php echo esc_url( $return_url ); ?>" class="button wc-auth-deny"><?php _e( 'Deny', 'woocommerce' ); ?></a>
+  <a href="<?php echo esc_url( $granted_url ); ?>" class="button button-primary wc-auth-approve"><?php _e( 'Approve', 'woocommerce' ); ?></a>
+  <a href="<?php echo esc_url( $return_url ); ?>" class="button wc-auth-deny"><?php _e( 'Deny', 'woocommerce' ); ?></a>
 </p>
 
 <?php do_action( 'woocommerce_auth_page_footer' ); ?>
