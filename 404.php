d27072bd483add8dcedecc0d6212011fa24a9204
PJ Hampton <hampton-p1@email.ulster.ac.uk>
2016-06-01 19:40:50 +0100

    added outline to 404.

--- 404.php
+++ 404.php
@@ -22,7 +22,7 @@
   <div class="text-center">
 
     <h1>404</h1>
-    <a class="btn btn-danger" href="<?php echo get_home_url(); ?>">
+    <a class="btn btn-danger-outline" href="<?php echo get_home_url(); ?>">
       Return Home
     </a>
 
