3cfcbe717913c740208d5c07f37fc16a083afddc
PJ Hampton <hampton-p1@email.ulster.ac.uk>
2016-06-05 00:45:06 +0100

    added new args to build out comment form.

--- comments.php
+++ comments.php
@@ -78,11 +78,51 @@ if ( post_password_required() ) {
   <p class="no-comments"><?php _e( 'Comments are closed.', 'twentysixteen' ); ?></p>
   <?php endif; ?>
 
-  <?php
-    comment_form( array(
-    'title_reply_before' => '<h3 id="reply-title" class="comment-reply-title">',
-    'title_reply_after'  => '</h3>',
-    ) );
-  ?>
+<?php
+
+$args = array(
+  'id_form'           => 'commentform',
+  'class_form'      => 'comment-form',
+  'id_submit'         => 'submit',
+  'class_submit'      => 'submit btn',
+  'name_submit'       => 'submit',
+  'title_reply'       => __( 'Leave a Reply' ),
+  'title_reply_to'    => __( 'Leave a Reply to %s' ),
+  'cancel_reply_link' => __( 'Cancel Reply' ),
+  'label_submit'      => __( 'Post Comment' ),
+  'format'            => 'html',
+
+
+  'must_log_in' => '<label class="info must-log-in">' .
+    sprintf(
+      __( 'You must be <a href="%s">logged in</a> to post a comment.' ),
+      wp_login_url( apply_filters( 'the_permalink', get_permalink() ) )
+    ) . '</label>',
+
+  'logged_in_as' => '<p class="logged-in-as">' .
+    sprintf(
+    __( 'Logged in as <a href="%1$s">%2$s</a>. <a href="%3$s" title="Log out of this account">Log out?</a>' ),
+      admin_url( 'profile.php' ),
+      $user_identity,
+      wp_logout_url( apply_filters( 'the_permalink', get_permalink( ) ) )
+    ) . '</p>',
+
+  'comment_notes_before' => '<p class="comment-notes">' .
+    __( 'Your email address will not be published.' ) . ( $req ? $required_text : '' ) .
+    '</p>',
+
+  'comment_notes_after' => '<p class="form-allowed-tags">' .
+    sprintf(
+      __( 'You may use these <abbr title="HyperText Markup Language">HTML</abbr> tags and attributes: %s' ),
+      ' <code>' . allowed_tags() . '</code>'
+    ) . '</p>',
+
+);
+
+?>
+
+<?php
+  comment_form( $args );
+?>
 
 </div>
