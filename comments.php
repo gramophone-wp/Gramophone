e33e9aecde59940f66e81816049cc08ac2b7b3d7
PJ Hampton <hampton-p1@email.ulster.ac.uk>
2016-06-06 22:35:45 +0100

    Updated comment form.

--- comments.php
+++ comments.php
@@ -82,15 +82,38 @@ if ( post_password_required() ) {
 
 $args = array(
   'id_form'           => 'commentform',
-  'class_form'      => 'comment-form',
+  'class_form'        => 'comment-form',
   'id_submit'         => 'submit',
-  'class_submit'      => 'submit btn',
+  'class_submit'      => 'submit btn btn-block',
   'name_submit'       => 'submit',
-  'title_reply'       => __( 'Leave a Reply' ),
+  'title_reply'       => __( 'Leave a Reply'       ),
   'title_reply_to'    => __( 'Leave a Reply to %s' ),
-  'cancel_reply_link' => __( 'Cancel Reply' ),
-  'label_submit'      => __( 'Post Comment' ),
+  'cancel_reply_link' => __( 'Cancel Reply'        ),
+  'label_submit'      => __( 'Post Comment'        ),
   'format'            => 'html',
+
+  'comment_field' => '<p class="comment-form-comment"><label for="comment">' . _x( 'Comment', 'noun' ) . '</label><textarea class="form-control" id="comment" name="comment" cols="45" rows="8" aria-required="true"></textarea></p>',
+  'fields' => apply_filters( 'comment_form_default_fields', array(
+    'author' =>
+      '<p class="comment-form-author">' .
+      '<label for="author">' . __( 'Name', 'domainreference' ) . '</label> ' .
+      ( $req ? '<span class="required">*</span>' : '' ) .
+      '<input class="form-control" id="author" name="author" type="text" value="' . esc_attr( $commenter['comment_author'] ) .
+      '" size="30"' . $aria_req . ' /></p>',
+
+    'email' =>
+      '<p class="comment-form-email"><label for="email">' . __( 'Email', 'domainreference' ) . '</label> ' .
+      ( $req ? '<span class="required">*</span>' : '' ) .
+      '<input class="form-control" id="email" name="email" type="text" value="' . esc_attr(  $commenter['comment_author_email'] ) .
+      '" size="30"' . $aria_req . ' /></p>',
+
+    'url' =>
+      '<p class="comment-form-url"><label for="url">' .
+      __( 'Website', 'domainreference' ) . '</label>' .
+      '<input class="form-control" id="url" name="url" type="text" value="' . esc_attr( $commenter['comment_author_url'] ) .
+      '" size="30" /></p>'
+
+  )),
 
 
   'must_log_in' => '<label class="info must-log-in">' .
