37622fb72a84fe0b6513c1df77b816ce0e3cdd54
PJ Hampton <hampton-p1@email.ulster.ac.uk>
2016-06-13 16:09:22 +0100

    made dialog dismissable.

--- woocommerce/notices/success.php
+++ woocommerce/notices/success.php
@@ -26,5 +26,10 @@ if ( ! $messages ){
 ?>
 
 <?php foreach ( $messages as $message ) : ?>
-  <div class="woocommerce-message alert alert-info"><?php echo wp_kses_post( $message ); ?></div>
+  <div class="woocommerce-message alert alert-info alert-dismissible" role="alert">
+    <button type="button" class="close" data-dismiss="alert" aria-label="close">
+      <span aria-hidden="true">&times;</span>
+    </button> 
+    <?php echo wp_kses_post( $message ); ?>
+  </div>
 <?php endforeach; ?>
