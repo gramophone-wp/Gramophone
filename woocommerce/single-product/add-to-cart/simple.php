196b5f932848095618cbb6ed200c3d3286f99a3f
PJ Hampton <hampton-p1@email.ulster.ac.uk>
2016-06-15 13:56:40 +0100

    Updated 'in stock' style.

--- woocommerce/single-product/add-to-cart/simple.php
+++ woocommerce/single-product/add-to-cart/simple.php
@@ -30,7 +30,7 @@ if ( ! $product->is_purchasable() ) {
 <?php
   // Availability
   $availability      = $product->get_availability();
-  $availability_html = empty( $availability['availability'] ) ? '' : '<p class="stock ' . esc_attr( $availability['class'] ) . '">' . esc_html( $availability['availability'] ) . '</p>';
+  $availability_html = empty( $availability['availability'] ) ? '' : '<p class="stock ' . esc_attr( $availability['class'] ) . '"><strong>' . esc_html( $availability['availability'] ) . '</strong></p>';
 
   echo apply_filters( 'woocommerce_stock_html', $availability_html, $availability['availability'], $product );
 ?>
