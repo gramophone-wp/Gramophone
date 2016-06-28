59f8a27e75f25ee9491c1bb5249b9f576634c5b8
PJ Hampton <hampton-p1@email.ulster.ac.uk>
2016-06-28 21:38:30 +0100

    removed header from woocommerce shop

--- woocommerce.php
+++ woocommerce.php
@@ -18,13 +18,7 @@
 
 <?php get_header(); ?>
 
-<div class="jumbotron">
-  <div class="container">
-    <h1><?php the_title(); ?></h1>
-  </div>
-</div>
-
-<div class="container">   
+<div id="woocommerce" class="container">   
   <div class="row row-offcanvas row-offcanvas-right">
     <div class="col-md-12 text-justify">
     <?php woocommerce_content(); ?>          
