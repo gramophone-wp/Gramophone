bd52e6ec5fec91add6ff3de8bf3ff95f5d2c2a95
PJ Hampton <hampton-p1@email.ulster.ac.uk>
2016-08-20 23:41:56 +0100

    Added shink-to-fit option.

--- header.php
+++ header.php
@@ -25,7 +25,7 @@
 
   <meta charset="<?php bloginfo( 'charset' ); ?>">
   <meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <link rel="shortcut icon" href="<?php esc_url(get_template_directory_uri()) ?>lib/images/favicons/favicon.ico">
 
   <?php if ( is_singular() && pings_open( get_queried_object() ) ) : ?>
