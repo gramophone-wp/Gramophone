12deef9facd47c09cb9b59383e7453067e51c50d
PJ Hampton <hampton-p1@email.ulster.ac.uk>
2016-08-24 11:05:06 +0100

    Updated formatting to 4 spaces as requested by users.

--- woocommerce/content-product.php
+++ woocommerce/content-product.php
@@ -17,24 +17,24 @@
 **/
 
 if ( ! defined( 'ABSPATH' ) ) {
-  exit; // exit if accessed directly
+    exit; // exit if accessed directly
 }
 
 global $product, $woocommerce_loop;
 
 // Store loop count we're currently on
 if ( empty( $woocommerce_loop['loop'] ) ) {
-  $woocommerce_loop['loop'] = 0;
+    $woocommerce_loop['loop'] = 0;
 }
 
 // Store column count for displaying the grid
 if ( empty( $woocommerce_loop['columns'] ) ) {
-  $woocommerce_loop['columns'] = apply_filters( 'loop_shop_columns', 4 );
+    $woocommerce_loop['columns'] = apply_filters( 'loop_shop_columns', 4 );
 }
 
 // Ensure visibility
 if ( ! $product || ! $product->is_visible() ) {
-  return;
+    return;
 }
 
 // Increase loop count
@@ -42,55 +42,60 @@ $woocommerce_loop['loop']++;
 
 // Extra post classes
 $classes = array();
-if ( 0 === ( $woocommerce_loop['loop'] - 1 ) % $woocommerce_loop['columns'] || 1 === $woocommerce_loop['columns'] ) {
-  $classes[] = 'first';
+if ( 0 === ( $woocommerce_loop['loop'] - 1 ) % $woocommerce_loop['columns'] || 1 === $woocommerce_loop['columns'] ) 
+{
+    $classes[] = 'first';
 }
-if ( 0 === $woocommerce_loop['loop'] % $woocommerce_loop['columns'] ) {
-  $classes[] = 'last';
+
+if ( 0 === $woocommerce_loop['loop'] % $woocommerce_loop['columns'] ) ]
+{
+    $classes[] = 'last';
 }
+
 ?>
+
 <div class="col-xs-12 col-sm-6 col-md-3" <?php post_class( $classes ); ?>>
-  <div class="card">
+    <div class="card">
 
-  <?php
-  /**
-   * woocommerce_before_shop_loop_item hook.
-   *
-   * @hooked woocommerce_template_loop_product_link_open - 10
-   */
-  do_action( 'woocommerce_before_shop_loop_item' );
+    <?php
+        /**
+        * woocommerce_before_shop_loop_item hook.
+        *
+        * @hooked woocommerce_template_loop_product_link_open - 10
+        */
+        do_action( 'woocommerce_before_shop_loop_item' );
 
-  /**
-   * woocommerce_before_shop_loop_item_title hook.
-   *
-   * @hooked woocommerce_show_product_loop_sale_flash - 10
-   * @hooked woocommerce_template_loop_product_thumbnail - 10
-   */
-  do_action( 'woocommerce_before_shop_loop_item_title' );
+        /**
+        * woocommerce_before_shop_loop_item_title hook.
+        *
+        * @hooked woocommerce_show_product_loop_sale_flash - 10
+        * @hooked woocommerce_template_loop_product_thumbnail - 10
+        */
+        do_action( 'woocommerce_before_shop_loop_item_title' );
 
-  /**
-   * woocommerce_shop_loop_item_title hook.
-   *
-   * @hooked woocommerce_template_loop_product_title - 10
-   */
-  do_action( 'woocommerce_shop_loop_item_title' );
+        /**
+        * woocommerce_shop_loop_item_title hook.
+        *
+        * @hooked woocommerce_template_loop_product_title - 10
+        */
+        do_action( 'woocommerce_shop_loop_item_title' );
 
-  /**
-   * woocommerce_after_shop_loop_item_title hook.
-   *
-   * @hooked woocommerce_template_loop_rating - 5
-   * @hooked woocommerce_template_loop_price - 10
-   */
-  do_action( 'woocommerce_after_shop_loop_item_title' );
+        /**
+        * woocommerce_after_shop_loop_item_title hook.
+        *
+        * @hooked woocommerce_template_loop_rating - 5
+        * @hooked woocommerce_template_loop_price - 10
+        */
+        do_action( 'woocommerce_after_shop_loop_item_title' );
 
-  /**
-   * woocommerce_after_shop_loop_item hook.
-   *
-   * @hooked woocommerce_template_loop_product_link_close - 5
-   * @hooked woocommerce_template_loop_add_to_cart - 10
-   */
-  do_action( 'woocommerce_after_shop_loop_item' );
-  ?>
+        /**
+        * woocommerce_after_shop_loop_item hook.
+        *
+        * @hooked woocommerce_template_loop_product_link_close - 5
+        * @hooked woocommerce_template_loop_add_to_cart - 10
+        */
+        do_action( 'woocommerce_after_shop_loop_item' );
+    ?>
 
-  </div>
+    </div>
 </div>
--- woocommerce/single-product.php
+++ woocommerce/single-product.php
@@ -17,43 +17,43 @@
 **/
 
 if ( ! defined( 'ABSPATH' ) ) {
-  exit; // Exit if accessed directly
+    exit; // Exit if accessed directly
 }
 
 get_header( 'shop' ); ?>
 
-  <?php
+<?php
     /**
-     * woocommerce_before_main_content hook.
-     *
-     * @hooked woocommerce_output_content_wrapper - 10 (outputs opening divs for the content)
-     * @hooked woocommerce_breadcrumb - 20
-     */
+    * woocommerce_before_main_content hook.
+    *
+    * @hooked woocommerce_output_content_wrapper - 10 (outputs opening divs for the content)
+    * @hooked woocommerce_breadcrumb - 20
+    */
     do_action( 'woocommerce_before_main_content' );
-  ?>
+?>
 
-    <?php while ( have_posts() ) : the_post(); ?>
+<?php while ( have_posts() ) : the_post(); ?>
 
-      <?php wc_get_template_part( 'content', 'single-product' ); ?>
+    <?php wc_get_template_part( 'content', 'single-product' ); ?>
 
-    <?php endwhile; // end of the loop. ?>
+<?php endwhile; // end of the loop. ?>
 
-  <?php
+<?php
     /**
-     * woocommerce_after_main_content hook.
-     *
-     * @hooked woocommerce_output_content_wrapper_end - 10 (outputs closing divs for the content)
-     */
+    * woocommerce_after_main_content hook.
+    *
+    * @hooked woocommerce_output_content_wrapper_end - 10 (outputs closing divs for the content)
+    */
     do_action( 'woocommerce_after_main_content' );
-  ?>
+?>
 
-  <?php
+<?php
     /**
-     * woocommerce_sidebar hook.
-     *
-     * @hooked woocommerce_get_sidebar - 10
-     */
+    * woocommerce_sidebar hook.
+    *
+    * @hooked woocommerce_get_sidebar - 10
+    */
     do_action( 'woocommerce_sidebar' );
-  ?>
+?>
 
 <?php get_footer( 'shop' ); ?>
