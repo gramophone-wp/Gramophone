fcd2fc3ebee88a0dbd857923434d2e0761d93323
PJ Hampton <hampton-p1@email.ulster.ac.uk>
2016-08-25 21:49:54 +0100

    Updated formatting to 4 spaces as requested by users.

--- woocommerce/checkout/form-pay.php
+++ woocommerce/checkout/form-pay.php
@@ -15,73 +15,91 @@
  * @version
 **/
 
-if ( ! defined( 'ABSPATH' ) ) {
-  exit;
+if ( ! defined( 'ABSPATH' ) ) 
+{
+    exit;
 }
 
 ?>
+
 <form id="order_review" method="post">
 
-  <table class="shop_table">
-    <thead>
-      <tr>
-        <th class="product-name"><?php _e( 'Product', 'woocommerce' ); ?></th>
-        <th class="product-quantity"><?php _e( 'Qty', 'woocommerce' ); ?></th>
-        <th class="product-total"><?php _e( 'Totals', 'woocommerce' ); ?></th>
-      </tr>
-    </thead>
-    <tbody>
-      <?php if ( sizeof( $order->get_items() ) > 0 ) : ?>
-        <?php foreach ( $order->get_items() as $item ) : ?>
-          <tr>
-            <td class="product-name">
-              <?php echo esc_html( $item['name'] ); ?>
-              <?php $order->display_item_meta( $item ); ?>
-            </td>
-            <td class="product-quantity"><?php echo esc_html( $item['qty'] ); ?></td>
-            <td class="product-subtotal"><?php echo $order->get_formatted_line_subtotal( $item ); ?></td>
-          </tr>
-        <?php endforeach; ?>
-      <?php endif; ?>
-    </tbody>
-    <tfoot>
-      <?php if ( $totals = $order->get_order_item_totals() ) : ?>
-        <?php foreach ( $totals as $total ) : ?>
-          <tr>
-            <th scope="row" colspan="2"><?php echo $total['label']; ?></th>
-            <td class="product-total"><?php echo $total['value']; ?></td>
-          </tr>
-        <?php endforeach; ?>
-      <?php endif; ?>
-    </tfoot>
-  </table>
+    <table class="shop_table">
 
-  <div id="payment">
-    <?php if ( $order->needs_payment() ) : ?>
-      <ul class="wc_payment_methods payment_methods methods">
+        <thead>
+            <tr>
+                <th class="product-name"><?php _e( 'Product', 'woocommerce' ); ?></th>
+                <th class="product-quantity"><?php _e( 'Qty', 'woocommerce' ); ?></th>
+                <th class="product-total"><?php _e( 'Totals', 'woocommerce' ); ?></th>
+            </tr>
+        </thead>
+
+        <tbody>
+            <?php if ( sizeof( $order->get_items() ) > 0 ) : ?>
+                <?php foreach ( $order->get_items() as $item ) : ?>
+                <tr>
+                    <td class="product-name">
+                    <?php echo esc_html( $item['name'] ); ?>
+                    <?php $order->display_item_meta( $item ); ?>
+                    </td>
+                    <td class="product-quantity"><?php echo esc_html( $item['qty'] ); ?></td>
+                    <td class="product-subtotal"><?php echo $order->get_formatted_line_subtotal( $item ); ?></td>
+                </tr>
+                <?php endforeach; ?>
+            <?php endif; ?>
+        </tbody>
+
+        <tfoot>
+        <?php if ( $totals = $order->get_order_item_totals() ) : ?>
+            <?php foreach ( $totals as $total ) : ?>
+                <tr>
+                    <th scope="row" colspan="2"><?php echo $total['label']; ?></th>
+                    <td class="product-total"><?php echo $total['value']; ?></td>
+                </tr>
+            <?php endforeach; ?>
+        <?php endif; ?>
+        </tfoot>
+
+    </table>
+
+    <div id="payment">
+
+        <?php if ( $order->needs_payment() ) : ?>
+        <ul class="wc_payment_methods payment_methods methods">
+
         <?php
-          if ( ! empty( $available_gateways ) ) {
-            foreach ( $available_gateways as $gateway ) {
-              wc_get_template( 'checkout/payment-method.php', array( 'gateway' => $gateway ) );
+        if ( ! empty( $available_gateways ) ) 
+        {
+            foreach ( $available_gateways as $gateway ) 
+            {
+            wc_get_template( 'checkout/payment-method.php', array( 'gateway' => $gateway ) );
             }
-          } else {
+        } 
+        else 
+        {
             echo '<li>' . apply_filters( 'woocommerce_no_available_payment_methods_message', __( 'Sorry, it seems that there are no available payment methods for your location. Please contact us if you require assistance or wish to make alternate arrangements.', 'woocommerce' ) ) . '</li>';
-          }
+        }
+
         ?>
-      </ul>
-    <?php endif; ?>
-    <div class="form-row">
-      <input type="hidden" name="woocommerce_pay" value="1" />
 
-      <?php wc_get_template( 'checkout/terms.php' ); ?>
+        </ul>
 
-      <?php do_action( 'woocommerce_pay_order_before_submit' ); ?>
+        <?php endif; ?>
+        
+        <div class="form-row">
+            <input type="hidden" name="woocommerce_pay" value="1" />
 
-      <?php echo apply_filters( 'woocommerce_pay_order_button_html', '<input type="submit" class="button alt" id="place_order" value="' . esc_attr( $order_button_text ) . '" data-value="' . esc_attr( $order_button_text ) . '" />' ); ?>
+            <?php wc_get_template( 'checkout/terms.php' ); ?>
 
-      <?php do_action( 'woocommerce_pay_order_after_submit' ); ?>
+            <?php do_action( 'woocommerce_pay_order_before_submit' ); ?>
 
-      <?php wp_nonce_field( 'woocommerce-pay' ); ?>
+            <?php echo apply_filters( 'woocommerce_pay_order_button_html', '<input type="submit" class="button alt" id="place_order" value="' . esc_attr( $order_button_text ) . '" data-value="' . esc_attr( $order_button_text ) . '" />' ); ?>
+
+            <?php do_action( 'woocommerce_pay_order_after_submit' ); ?>
+
+            <?php wp_nonce_field( 'woocommerce-pay' ); ?>
+        </div>
+
     </div>
-  </div>
+
 </form>
