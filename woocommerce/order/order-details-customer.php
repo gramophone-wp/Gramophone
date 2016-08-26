f43c0d4d6dd739b9d2de37e77c485aeb759c47f3
PJ Hampton <hampton-p1@email.ulster.ac.uk>
2016-08-26 11:43:17 +0100

    Updated formatting to 4 spaces as requested by users.

--- woocommerce/order/order-details-customer.php
+++ woocommerce/order/order-details-customer.php
@@ -16,62 +16,69 @@
 **/
 
 
-if ( ! defined( 'ABSPATH' ) ) {
-  exit;
+if ( ! defined( 'ABSPATH' ) ) 
+{
+    exit;
 }
+
 ?>
+
 <header><h2><?php _e( 'Customer Details', 'woocommerce' ); ?></h2></header>
 
 <table class="shop_table customer_details">
-  <?php if ( $order->customer_note ) : ?>
+    <?php if ( $order->customer_note ) : ?>
     <tr>
-      <th><?php _e( 'Note:', 'woocommerce' ); ?></th>
-      <td><?php echo wptexturize( $order->customer_note ); ?></td>
+        <th><?php _e( 'Note:', 'woocommerce' ); ?></th>
+        <td><?php echo wptexturize( $order->customer_note ); ?></td>
     </tr>
-  <?php endif; ?>
+    <?php endif; ?>
 
-  <?php if ( $order->billing_email ) : ?>
+    <?php if ( $order->billing_email ) : ?>
     <tr>
-      <th><?php _e( 'Email:', 'woocommerce' ); ?></th>
-      <td><?php echo esc_html( $order->billing_email ); ?></td>
+        <th><?php _e( 'Email:', 'woocommerce' ); ?></th>
+        <td><?php echo esc_html( $order->billing_email ); ?></td>
     </tr>
-  <?php endif; ?>
+    <?php endif; ?>
 
-  <?php if ( $order->billing_phone ) : ?>
+    <?php if ( $order->billing_phone ) : ?>
     <tr>
-      <th><?php _e( 'Telephone:', 'woocommerce' ); ?></th>
-      <td><?php echo esc_html( $order->billing_phone ); ?></td>
+        <th><?php _e( 'Telephone:', 'woocommerce' ); ?></th>
+        <td><?php echo esc_html( $order->billing_phone ); ?></td>
     </tr>
-  <?php endif; ?>
+    <?php endif; ?>
 
-  <?php do_action( 'woocommerce_order_details_after_customer_details', $order ); ?>
+    <?php do_action( 'woocommerce_order_details_after_customer_details', $order ); ?>
 </table>
 
 <?php if ( ! wc_ship_to_billing_address_only() && $order->needs_shipping_address() ) : ?>
 
 <div class="col2-set addresses">
-  <div class="col-1">
+    <div class="col-1">
 
-<?php endif; ?>
+        <?php endif; ?>
 
-<header class="title">
-  <h3><?php _e( 'Billing Address', 'woocommerce' ); ?></h3>
-</header>
-<address>
-  <?php echo ( $address = $order->get_formatted_billing_address() ) ? $address : __( 'N/A', 'woocommerce' ); ?>
-</address>
+        <header class="title">
+            <h3><?php _e( 'Billing Address', 'woocommerce' ); ?></h3>
+        </header>
 
-<?php if ( ! wc_ship_to_billing_address_only() && $order->needs_shipping_address() ) : ?>
+        <address>
+            <?php echo ( $address = $order->get_formatted_billing_address() ) ? $address : __( 'N/A', 'woocommerce' ); ?>
+        </address>
 
-  </div><!-- /.col-1 -->
-  <div class="col-2">
-    <header class="title">
-      <h3><?php _e( 'Shipping Address', 'woocommerce' ); ?></h3>
-    </header>
-    <address>
-      <?php echo ( $address = $order->get_formatted_shipping_address() ) ? $address : __( 'N/A', 'woocommerce' ); ?>
-    </address>
-  </div><!-- /.col-2 -->
+        <?php if ( ! wc_ship_to_billing_address_only() && $order->needs_shipping_address() ) : ?>
+
+    </div><!-- /.col-1 -->
+
+    <div class="col-2">
+        <header class="title">
+            <h3><?php _e( 'Shipping Address', 'woocommerce' ); ?></h3>
+        </header>
+        
+        <address>
+            <?php echo ( $address = $order->get_formatted_shipping_address() ) ? $address : __( 'N/A', 'woocommerce' ); ?>
+        </address>
+    </div><!-- /.col-2 -->
+
 </div><!-- /.col2-set -->
 
 <?php endif; ?>
--- woocommerce/order/order-details.php
+++ woocommerce/order/order-details.php
@@ -15,8 +15,9 @@
  * @version
 **/
 
-if ( ! defined( 'ABSPATH' ) ) {
-  exit;
+if ( ! defined( 'ABSPATH' ) ) 
+{
+    exit;
 }
 
 $order = wc_get_order( $order_id );
@@ -27,47 +28,54 @@ $show_customer_details = is_user_logged_in() && $order->get_user_id() === get_cu
 ?>
 
 <h2><?php _e( 'Order Details', 'woocommerce' ); ?></h2>
-<table class="shop_table order_details">
-  <thead>
-    <tr>
-      <th class="product-name"><?php _e( 'Product', 'woocommerce' ); ?></th>
-      <th class="product-total"><?php _e( 'Total', 'woocommerce' ); ?></th>
-    </tr>
-  </thead>
-  <tbody>
-    <?php
-      foreach( $order->get_items() as $item_id => $item ) {
-        $product = apply_filters( 'woocommerce_order_item_product', $order->get_product_from_item( $item ), $item );
-        $purchase_note = get_post_meta( $product->id, '_purchase_note', true );
 
-        wc_get_template( 'order/order-details-item.php', array(
-          'order'         => $order,
-          'item_id'       => $item_id,
-          'item'          => $item,
-          'show_purchase_note'  => $show_purchase_note,
-          'purchase_note'     => $purchase_note,
-          'product'       => $product,
-        ) );
-      }
-    ?>
-    <?php do_action( 'woocommerce_order_items_table', $order ); ?>
-  </tbody>
-  <tfoot>
-    <?php
-      foreach ( $order->get_order_item_totals() as $key => $total ) {
-        ?>
+<table class="shop_table order_details">
+
+    <thead>
         <tr>
-          <th scope="row"><?php echo $total['label']; ?></th>
-          <td><?php echo $total['value']; ?></td>
+            <th class="product-name"><?php _e( 'Product', 'woocommerce' ); ?></th>
+            <th class="product-total"><?php _e( 'Total', 'woocommerce' ); ?></th>
         </tr>
+    </thead>
+
+    <tbody>
         <?php
-      }
-    ?>
-  </tfoot>
+            foreach( $order->get_items() as $item_id => $item ) 
+            {
+                $product = apply_filters( 'woocommerce_order_item_product', $order->get_product_from_item( $item ), $item );
+                $purchase_note = get_post_meta( $product->id, '_purchase_note', true );
+
+                wc_get_template( 'order/order-details-item.php', array(
+                'order'         => $order,
+                'item_id'       => $item_id,
+                'item'          => $item,
+                'show_purchase_note'  => $show_purchase_note,
+                'purchase_note'     => $purchase_note,
+                'product'       => $product,
+                ) );
+            }
+        ?>
+        <?php do_action( 'woocommerce_order_items_table', $order ); ?>
+    </tbody>
+
+    <tfoot>
+        <?php
+            foreach ( $order->get_order_item_totals() as $key => $total ) 
+            {
+            ?>
+                <tr>
+                    <th scope="row"><?php echo $total['label']; ?></th>
+                    <td><?php echo $total['value']; ?></td>
+                </tr>
+            <?php
+            }
+        ?>
+    </tfoot>
+
 </table>
 
 <?php do_action( 'woocommerce_order_details_after_order_table', $order ); ?>
 
 <?php if ( $show_customer_details ) : ?>
-  <?php wc_get_template( 'order/order-details-customer.php', array( 'order' =>  $order ) ); ?>
+    <?php wc_get_template( 'order/order-details-customer.php', array( 'order' =>  $order ) ); ?>
 <?php endif; ?>
