cfd169e8f909f63af3fd81f5a7578e922a2ecd54
PJ Hampton <pjhampton@protonmail.com>
2017-02-19 20:17:17 +0000

    Updated nav to BS4 nav.

--- header.php
+++ header.php
@@ -41,31 +41,43 @@
 
 <body <?php body_class(); ?> <?php post_class('title_post group'); ?>>
 
-    <nav class="navbar navbar-toggleable-md navbar-inverse bg-inverse">
-      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="container">
-      <a class="navbar-brand" href="<?php echo esc_url(home_url()); ?>"><?php bloginfo( 'name' ); ?></a>
+<header class="navbar navbar-light navbar-toggleable-md bd-navbar">
+    <nav class="container">
 
-      <div class="collapse navbar-collapse">
-
-        <?php
-            wp_nav_menu( array(
-                'menu'           => 'primary',
-                'theme_location' => 'primary',
-                'depth'          => 1,
-                'container'      => "ul",
-                'menu_class'     => "navbar-nav mr-auto",
-                'fallback_cb'    => 'bootstrap_walker::fallback',
-                'walker'         => new Bootstrap_Walker()
-            ) );
-        ?>
-
-        <form class="form-inline hidden-xs-down pull-right" method="get" action="<?php echo home_url( '/' ); ?>">
-            <input name="s" id="s" class="form-control" type="text" placeholder="Search">
-            <button class="btn btn-outline-info" type="submit">Search</button>
-        </form>
+        <div class="d-flex justify-content-between hidden-lg-up">
+            <a class="navbar-brand" href="<?php echo esc_url(home_url()); ?>">
+            <?php bloginfo( 'name' ); ?>
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bd-main-nav" aria-controls="bd-main-nav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
         </div>
-      </div>
+
+        <div class="collapse navbar-collapse" id="bd-main-nav">
+            <ul class="nav navbar-nav mr-auto">
+
+                <li class="nav-item active hidden-md-down">
+                    <a class="nav-item nav-link active" href="<?php echo esc_url(home_url()); ?>"><?php bloginfo( 'name' ); ?></a>
+                </li>
+
+                <?php
+                    wp_nav_menu( array(
+                        'menu'           => 'primary',
+                        'theme_location' => 'primary',
+                        'depth'          => 1,
+                        'container'      => "ul",
+                        'menu_class'     => "navbar-nav mr-auto",
+                        'fallback_cb'    => 'bootstrap_walker::fallback',
+                        'walker'         => new Bootstrap_Walker()
+                    ) );
+                ?>
+
+            </ul>
+
+            <form class="form-inline float-right hidden-md-down" method="get" action="<?php echo home_url( '/' ); ?>">
+                <input name="s" id="s" class="form-control" type="text" placeholder="Search">
+                <button class="btn btn-outline-info" type="submit">Search</button>
+            </form>
+        </div>
     </nav>
+</header>
