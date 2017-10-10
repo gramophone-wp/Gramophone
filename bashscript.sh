05ac0094a2c0f9f1e81c2e37cd8f38617338a12f
Matej Plavevski <matej.plavevski+github@gmail.com>
2017-10-10 15:13:13 +0200

    Create bashscript.sh

---
+++ bashscript.sh
@@ -0,0 +1,11 @@
+# To Do. give it a better name
+
+cp -a ../Gramophone/. /Gramaphone/gramaphonebackup/
+
+rm -rf .*
+
+rm -rf bashscript.sh	
+
+rm -rf gramaphonebackup
+
+zip ../Gramophone.zip ../Gramaphone
