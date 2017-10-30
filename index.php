9923d9606193268e3fb0a48d4648f6ba7537e138
Dilka Dilukshi <dilkadilukshi@gmail.com>
2017-10-30 14:02:11 -0700

    Change order

--- index.php
+++ index.php
@@ -19,20 +19,30 @@
 **/
 ?>
 
-<?php get_header(); ?>
+<?php 
+    get_header();
+?>
 
 <div class="container">
     <div class="row">
         <div class="col-md-9">
 
             <!-- The WordPress Loop -->
-            <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+            <?php 
+                if ( have_posts() ) : while ( have_posts() ) : the_post(); 
+            ?>
 
-            <h1><?php the_title(); ?></h1>
+            <h1>
+                <?php the_title(); ?>
+            </h1>
 
-            <?php the_content(); ?>
+            <?php 
+                the_content();
+            ?>
 
-            <?php endwhile; else: ?>
+            <?php 
+                endwhile; else: 
+            ?>
 
             <div class="page-header">
                 <h1>Oh no!</h1>
