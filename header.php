be12bd0b34bfef2a4108b49ca58603e2f0738afc
PJ Hampton <pjhampton@protonmail.com>
2017-11-04 19:11:25 +0000

    Fix header.

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <?php
 /**
  *******************************************************************************
- * //header.php
+ * ./header.php
  *******************************************************************************
  *
  * The header.
@@ -41,27 +41,14 @@
 
 <body <?php body_class(); ?> <?php post_class('title_post group'); ?>>
 
-<header class="navbar navbar-light navbar-toggleable-md bd-navbar">
-
-<!--
-    <nav class="container">
-
-        <div class="d-flex justify-content-between hidden-lg-up">
-            <a class="navbar-brand" href="<?php echo esc_url(home_url()); ?>">
-            <?php bloginfo( 'name' ); ?>
-            </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bd-main-nav" aria-controls="bd-main-nav" aria-expanded="false" aria-label="Toggle navigation">
+    <nav class="navbar navbar-expand-lg navbar-light bg-ligh">
+        <a class="navbar-brand" href="<?php echo esc_url(home_url()); ?>"><?php bloginfo( 'name' ); ?></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
-            </button>
-        </div>
-
-        <div class="collapse navbar-collapse" id="bd-main-nav">
-            <ul class="nav navbar-nav mr-auto">
-
-                <li class="nav-item active hidden-md-down">
-                    <a class="nav-item nav-link active" href="<?php echo esc_url(home_url()); ?>"><?php bloginfo( 'name' ); ?></a>
-                </li>
-
+        </button>
+  
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
                 <?php
                     wp_nav_menu( array(
                         'menu'           => 'primary',
@@ -73,50 +60,10 @@
                         'walker'         => new Bootstrap_Walker()
                     ) );
                 ?>
-
             </ul>
-
-            <form class="form-inline float-right hidden-md-down" method="get" action="<?php echo home_url( '/' ); ?>">
-                <input name="s" id="s" class="form-control" type="text" placeholder="Search">
-                <button class="btn btn-outline-info btn-search" type="submit">Search</button>
+            <form class="form-inline my-2 my-lg-0" method="get" action="<?php echo home_url( '/' ); ?>" style="float: right">
+                <input name="s" id="s" class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-info my-2 my-sm-0" type="submit">Search</button>
             </form>
         </div>
     </nav>
-    -->
-
-    <nav class="navbar sticky-top navbar-light bg-ligh">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
-      </li>
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
-  </div>
-</nav>
-</header>
