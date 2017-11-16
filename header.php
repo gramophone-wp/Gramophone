69ec157e61897443b87b3c5bc9ae5aa82a6c78db
PJ Hampton <pjhampton@protonmail.com>
2017-11-16 20:47:27 +0000

    Update nav position

--- header.php
+++ header.php
@@ -42,28 +42,30 @@
 <body <?php body_class(); ?> <?php post_class('title_post group'); ?>>
 
     <nav class="navbar navbar-expand-lg navbar-light bg-ligh">
-        <a class="navbar-brand" href="<?php echo esc_url(home_url()); ?>"><?php bloginfo( 'name' ); ?></a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-  
-        <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-            <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-                <?php
-                    wp_nav_menu( array(
-                        'menu'           => 'primary',
-                        'theme_location' => 'primary',
-                        'depth'          => 1,
-                        'container'      => "ul",
-                        'menu_class'     => "navbar-nav mr-auto",
-                        'fallback_cb'    => 'bootstrap_walker::fallback',
-                        'walker'         => new Bootstrap_Walker()
-                    ) );
-                ?>
-            </ul>
-            <form class="form-inline my-2 my-lg-0" method="get" action="<?php echo home_url( '/' ); ?>" style="float: right">
-                <input name="s" id="s" class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-info my-2 my-sm-0" type="submit">Search</button>
-            </form>
+        <div class="container">
+            <a class="navbar-brand" href="<?php echo esc_url(home_url()); ?>"><?php bloginfo( 'name' ); ?></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+                <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                    <?php
+                        wp_nav_menu( array(
+                            'menu'           => 'primary',
+                            'theme_location' => 'primary',
+                            'depth'          => 1,
+                            'container'      => "ul",
+                            'menu_class'     => "navbar-nav mr-auto",
+                            'fallback_cb'    => 'bootstrap_walker::fallback',
+                            'walker'         => new Bootstrap_Walker()
+                        ) );
+                    ?>
+                </ul>
+                <form class="form-inline my-2 my-lg-0" method="get" action="<?php echo home_url( '/' ); ?>" style="float: right">
+                    <input name="s" id="s" class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-info my-2 my-sm-0" type="submit">Search</button>
+                </form>
+            </div>
         </div>
     </nav>
