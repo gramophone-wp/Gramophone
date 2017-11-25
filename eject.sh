5f3d6ee0e80500d5954f503f48209c8b9685d87d
PJ Hampton <pjhampton@protonmail.com>
2017-11-25 21:55:10 +0000

    Update the eject script.

--- eject.sh
+++ eject.sh
@@ -1,13 +1,23 @@
-cp -a ../Gramophone/. /Gramaphone/gramaphonebackup/
+echo "Copying dir"
+rm -rf dist/
+cp -R . ./../stage
+mv ../stage ./gramophone
 
-rm -rf .*
+declare -a remove_files=(
+  ".git"
+  "docker-compose.yml"
+  "README.md"
+  "eject.sh"
+)
 
-rm -rf docker-compose.yml
+echo "Removing files."
+for i in "${remove_files[@]}"
+do
+  echo "Removing $i"
+  rm -rf "./gramophone/$i"
+done
 
-rm -rf README.md
-
-rm -rf eject.sh	
-
-rm -rf gramaphonebackup
-
-zip ../Gramophone.zip ../Gramaphone
+echo "Making the theme"
+mkdir dist
+zip dist/gramophone.zip ./gramophone/*
+rm -rf ./gramophone
