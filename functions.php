d98d1b513215d2f93a79fd08c8e55b00716ab319
PJ Hampton <pjhampton@protonmail.com>
2017-12-01 20:34:20 +0000

    Add widgets to functions.php

--- functions.php
+++ functions.php
@@ -46,6 +46,83 @@ require_once( 'config/required_plugins.php' );
 // Theme custom post types
 // require_once( '' );
 
+/*
+ * Widgets
+ */
+
+function create_left_footer_widget()
+{
+    register_sidebar( array(
+        'name' => __( "Footer Left" ),
+        'id' => __("footer_left"),
+        'description' => __( "The left footer widget" ),
+        'before_widget' => '<div class="widget">',
+        'after_widget' => '</div>',
+        'before_title' => '<h4 class="text-center" id="widget-title">',
+        'after_title' => '</h4>'
+    ));
+}
+add_action( 'widgets_init', 'create_left_footer_widget' );
+
+
+function create_center_footer_widget()
+{
+    register_sidebar( array(
+        'name' => __( "Footer Center" ),
+        'id' => __("footer_center"),
+        'description' => __( "The center footer widget" ),
+        'before_widget' => '<div class="widget">',
+        'after_widget' => '</div>',
+        'before_title' => '<h4 class="text-center" id="widget-title">',
+        'after_title' => '</h4>'
+    ));
+}
+add_action( 'widgets_init', 'create_center_footer_widget' );
+
+
+function create_right_footer_widget()
+{
+    register_sidebar( array(
+        'name' => __( "Footer Right" ),
+        'id' => __("footer_right"),
+        'description' => __( "The right footer widget" ),
+        'before_widget' => '<div class="widget">',
+        'after_widget' => '</div>',
+        'before_title' => '<h4 class="text-center" id="widget-title">',
+        'after_title' => '</h4>'
+    ));
+}
+add_action( 'widgets_init', 'create_right_footer_widget' );
+
+
+function create_page_widget()
+{
+    register_sidebar( array(
+        'name' => __( "Page Widget" ),
+        'id' => __("page"),
+        'description' => __( "The page widget container" ),
+        'before_widget' => '<div class="widget">',
+        'after_widget' => '</div>',
+        'before_title' => '<h4 class="text-center" id="widget-title">',
+        'after_title' => '</h4>'
+    ));
+}
+add_action( 'widgets_init', 'create_page_widget' );
+
+
+function create_post_widget()
+{
+    register_sidebar( array(
+        'name' => __( "Post Widget" ),
+        'id' => __("post"),
+        'description' => __( "The post widget container" ),
+        'before_widget' => '<div class="widget">',
+        'after_widget' => '</div>',
+        'before_title' => '<h4 class="text-center" id="widget-title">',
+        'after_title' => '</h4>'
+    ));
+}
+add_action( 'widgets_init', 'create_page_widget' );
 /**
  **********************************************
  * add_theme_support
