67b47682ebaed5711ea56b13a3a7a13d261f0cbc
PJ Hampton <pjhampton@protonmail.com>
2017-12-03 21:25:18 +0000

    Add customer header support.

--- functions.php
+++ functions.php
@@ -126,11 +126,11 @@ add_action( 'widgets_init', 'create_page_widget' );
  */
 add_theme_support( 'automatic-feed-links' );
 add_theme_support( 'custom-background' );
+add_theme_support( 'custom-header' );
 add_theme_support( 'menus' );
 add_theme_support( 'post-thumbnails' );
 add_theme_support( 'post-formats', $post_formats );
 add_theme_support( 'title-tag' );
-// add_theme_support( 'custom-header' );
 // add_theme_support( 'editor-style' );
 // add_theme_support( 'html5' );
 // add_theme_support( 'widgets' );
