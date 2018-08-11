19cd35e6b28f245ead9805fd81e579fb92c8edf3
PJ Hampton <pjhampton@protonmail.com>
2018-08-11 20:02:02 +0100

    [pjh] Add jscrc

--- front-page.php
+++ front-page.php
@@ -2,9 +2,7 @@
 /**
  * The front page.
  *
- * CODEX REF
- * https://developer.wordpress.org/themes/basics/template-hierarchy/#front-page-display
- *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#front-page-display
  * @package gramophone
 **/
 ?>
@@ -13,36 +11,36 @@
 
 <div class="jumbotron jumbotron-fluid">
 
-    <div class="container">
-        <h1><?php echo get_bloginfo('name'); ?></h1>
-        <p><?php echo get_bloginfo('description'); ?></p>
-        </div>
-    </div>
+	<div class="container">
+		<h1><?php echo get_bloginfo('name'); ?></h1>
+		<p><?php echo get_bloginfo('description'); ?></p>
+		</div>
+	</div>
 
-    <div class="container" id="home">
-        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-        <?php the_content(); ?>
-        <?php endwhile; endif; ?>
-    </div>
+	<div class="container" id="home">
+		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+		<?php the_content(); ?>
+		<?php endwhile; endif; ?>
+	</div>
 
-    <div class="container">
-        <div class="row">
-            <!-- widget area 1 -->
-            <div class="col-md-4">
-            <?php if ( dynamic_sidebar( 'front-left' ) ); ?>
-            </div>
+	<div class="container">
+		<div class="row">
+			<!-- widget area 1 -->
+			<div class="col-md-4">
+			<?php if ( dynamic_sidebar( 'front-left' ) ); ?>
+			</div>
 
-            <!-- widget area 2 -->
-            <div class="col-md-4">
-            <?php if ( dynamic_sidebar( 'front-center' ) ); ?>
-            </div>
+			<!-- widget area 2 -->
+			<div class="col-md-4">
+			<?php if ( dynamic_sidebar( 'front-center' ) ); ?>
+			</div>
 
-            <!-- widget area 3 -->
-            <div class="col-md-4">
-            <?php if ( dynamic_sidebar( 'front-right' ) ); ?>
-            </div>
-        </div>
-    </div>
+			<!-- widget area 3 -->
+			<div class="col-md-4">
+			<?php if ( dynamic_sidebar( 'front-right' ) ); ?>
+			</div>
+		</div>
+	</div>
 </div>
 
 <?php get_footer(); ?>
