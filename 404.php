8d8592ceee49b50e5eca9b890a0afd51e636a2d1
PJ Hampton <pjhampton@protonmail.com>
2018-08-15 21:45:56 +0100

    [pjh] add mvp 404 page.

--- 404.php
+++ 404.php
@@ -8,14 +8,22 @@
 
 ?>
 
-<div class="col-md-12">
-	<div class="text-center">
+<?php get_header(); ?>
 
-		<h1>404</h1>
-
-		<a class="btn btn-danger-outline" href="<?php echo esc_url( get_home_url() ); ?>">
-			Return Home
-		</a>
-
-	</div>
+<div class="error-page">
+<h2>Oops!</h2>
+<h1 class="404error"> 404 </h1>
+<h2>Not Found</h2>
+<div class="error-details">
+	Sorry, an error has occured. Requested page not found!
 </div>
+
+
+
+<div class="error-actions">
+	<a href="{{url('/')}}" class="btn btn-primary btn-lg"><span class="glyphicon glyphicon-home"></span>
+		Take Me Home </a><a href="{{url('/contact-us')}}" class="btn btn-default btn-lg"><span class="glyphicon glyphicon-envelope"></span> Contact Support </a>
+</div>
+</div>
+
+
